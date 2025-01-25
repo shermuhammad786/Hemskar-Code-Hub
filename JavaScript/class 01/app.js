0c67e556ea9ab27140c0b5e80ad40fa2004afb3f
shermuhammad786 <sherabro141@gmail.com>
2025-01-25 22:47:22 +0500

    javascript start class 01

---
+++ JavaScript/class 01/app.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+// alert("Hello World");
+
+// var number1 = 9;
+
+// var number2 = "10";
+
+// var name = "Hemskar"
+
+// var sum = number1 - name; // not a number NaN
+
+// alert(sum)
+
+
+// var nameOne = "Hemskar";
+
+// var num = 4;
+
+
+// alert(++num);
+//    1 + 4
+
+// var num2 = ++num + num++ + ++num + ++num;
+//    4      5   +  5   +    7   +   8  = 25
+
+// alert(num2)
+
+
+
+// var n = 2;
+// var m = 3;
+
+// var n2 = n++  //  2 + 1
+// var n3 = ++m  // 1 + 3
+// //  2   2   -  4  + 4  =  2
+// alert(n2) // 2
+// alert(n3) // 3
