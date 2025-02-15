1df4f49e357dfab81c93cbd6d9a6fc5c2ebe51f9
shermuhammad786 <sherabro141@gmail.com>
2025-02-16 00:25:37 +0500

    js class 06

---
+++ JavaScript/class 06/app.js
@@ -0,0 +1,76 @@
+// var name = "zeeshan";
+// var name2 = "HEMSKAR";
+// console.log(">>>> hemskar values===>>> ", name2.toLowerCase());
+// var capitalName = name.toUpperCase();
+// console.log('capitalName: ', capitalName);
+
+// var search = prompt("Search with name");
+// var firstChar = search.slice(0, 1);
+// console.log('firstChar: ', firstChar);
+
+// var otherChar = search.slice(1);
+// console.log('otherChar: ', otherChar);
+
+// var fullName = firstChar.toUpperCase() + otherChar.toLowerCase()
+
+// if (search.toLowerCase() === "zeeshan") {
+//     document.write(fullName + " Your Account Found");
+// } else {
+//     document.write(fullName + " Your Accout not found");
+// }
+
+///                                                 // 15
+// var description = "This is the course by the Zeeshan Code Hub and best opertunity Zeeshan Code Hub for my university Zeeshan Code Hub class fellows"
+
+// var findIndex = description.lastIndexOf("Zeeshan Code Hub")
+// console.log('findIndex: ', findIndex);
+
+// description = description.slice(0, findIndex) + "Hemskar Code Hub" + description.slice(findIndex + 16);
+// console.log('description: ', description);
+
+
+
+//                  20 + 15 = 35                                                     50 + 16
+// var changeWord;
+// for (var i = 0; i < description.length; i++) {
+//     if (description.slice(i, i + 16) === "Zeeshan Code Hub") {
+//         var changeWord = description.slice(0, i) + "Hemskar Code Hub" + description.slice(i + 16)
+//     }
+// }
+
+// console.log("changeWord", changeWord)
+
+
+
+// var description = "This is the course by the Zeeshan Code Hub , best opertunity Zeeshan Code Hub for my university Zeeshan Code Hub";
+//                        69  70
+// console.log(" first character ==>> ", description.slice(6, 7));
+// console.log(" first character ==>> ", description.charAt(description.length - 1));
+// for (var i = 0; i < description.length; i++) {
+//     if (description.charAt(i) === ",") {
+//         alert("comma find");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+// var description = "This is the course by the Zeeshan Code Hub , best opertunity Zeeshan Code Hub for my university Zeeshan Code Hub";
+// // var changeText = description.replace(/Zeeshan Code Hub/g, "Hemskar Code Hub")
+// var changeText = description.replaceAll("Zeeshan Code Hub", "Hemskar Code Hub")
+// console.log('changeText: ', changeText);
+
+
+
+// var num = 0.4
+// var roundNum = 0.5;
+// console.log("number", Math.round(num));
+// console.log("number \n Rounded", Math.round(roundNum));
+
+
+
+alert("hello world! \n happy coding")
+document.write("hello world! <br/> happy coding")
