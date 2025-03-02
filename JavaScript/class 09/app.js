b83c9f7c87ba2e430f6b0805f1d460dfcea9ea1a
shermuhammad786 <sherabro141@gmail.com>
2025-03-02 23:25:38 +0500

    class 09 and 10

---
+++ JavaScript/class 09/app.js
@@ -0,0 +1,108 @@
+// function calculateTotal(total) {
+//     var orderTot;
+//     // var total = 150;
+//     if (total >= 100) {
+//         orderTot = total;
+//     }
+//     else if (total < 50.01) {
+//         orderTot = total + 5;
+//     }
+//     else {
+//         orderTot = total + 5 + (.03 * (total - 50));
+//     }
+//     console.log(orderTot, " ===>>> orderTot")
+// }
+// // calculateTotal(50)
+// // calculateTotal(150)
+// calculateTotal("hemskar")
+// calculateTotal(20)
+
+// var orderTot;
+// var total = 150;
+// if (total >= 100) {
+//     orderTot = total;
+// }
+// else if (total < 50.01) {
+//     orderTot = total + 5;
+// }
+// else {
+//     orderTot = total + 5 + (.03 * (total - 50));
+// }
+// console.log(orderTot, " ===>>> orderTot")
+
+
+// var orderTot;
+// var total = 70;
+// if (total >= 100) {
+//     orderTot = total;
+// }
+// else if (total < 50.01) {
+//     orderTot = total + 5;
+// }
+// else {
+//     orderTot = total + 5 + (.03 * (total - 50));
+// }
+// console.log(orderTot, " ===>>> orderTot")
+
+
+// var orderTot;
+// var total = 20;
+// if (total >= 100) {
+//     orderTot = total;
+// }
+// else if (total < 50.01) {
+//     orderTot = total + 5;
+// }
+// else {
+//     orderTot = total + 5 + (.03 * (total - 50));
+// }
+// console.log(orderTot, " ===>>> orderTot")
+
+
+// var orderTot;
+// var total = 50;
+// if (total >= 100) {
+//     orderTot = total;
+// }
+// else if (total < 50.01) {
+//     orderTot = total + 5;
+// }
+// else {
+//     orderTot = total + 5 + (.03 * (total - 50));
+// }
+// console.log(orderTot, " ===>>> orderTot")
+
+
+
+// var number = 123;
+// number = "hemskar"
+
+
+// // var name = "Hemskar";
+// function returnSomeValue() {
+//     var name = "hemskar";
+//     var correctCondition;
+//     if (name === "hemskar") {
+//         correctCondition = name
+//     }
+//     return "Code Hub"
+// };
+
+// // var funVal = "Code Hub";
+// var funVal = returnSomeValue();
+// console.log('funVal: ', funVal);
+
+
+function myFun() {
+    var name = "hemskar";
+    var password = 123;
+    if (name === "hemskar" && password === 123) {
+        alert("Signup successfull")
+    }
+    return name;
+}
+
+myFun()
+var funValue = myFun();
+console.log('funValue: ', funValue);
+alert(funValue)
---
+++ JavaScript/class 10/app.js
@@ -0,0 +1,61 @@
+// var courseName;
+// function localVariable() {
+//     var courseName = "hemskar";
+//     console.log('courseName: ', courseName);
+// }
+// localVariable()
+// console.log("courseName ===>>> ", courseName);
+
+// undefined  ==>>> variable hai pr value nhii hai
+// not defined ====>> variable nhii milla
+
+
+// console.log("courseName ===>>> ", courseName);
+
+
+
+
+// var courseName = "Hemskar";
+// var newName = "Code Hub 01";
+// // var newName = "var with same name";
+// if (courseName === "Hemskar") {
+//     console.log('newName: 01 ', newName);
+//     var newName = "Code Hub";
+// }
+// console.log('newName: ', newName);
+
+
+// var courseName = "Hemskar global";
+// function globalVSlocal() {
+//     console.log('courseName local: ', courseName);
+//     courseName = "CodeHub"
+//     // var courseName = "Hemskar local";
+// }
+// globalVSlocal()
+// console.log('courseName: global ==>>', courseName);
+
+
+
+// console.log('courseName: top==>>> ', courseName);
+// var courseName = "Hemskar global";
+// console.log('courseName: bottom ==>>', courseName);
+
+
+var myName = "Shahbaz";
+var myName = "Zeeshan";
+myName = "Hina";
+var myName = "Muskan";
+myName = "Hemskar";
+
+console.log('myName: ', myName);
+
+
+// var myName = "Shahbaz";
+// function globalVSlocal() {
+//     if (myName === "Shahbaz") {
+//         var myName = "Zeeshan";
+//         console.log('myName: local', myName);
+//     }
+// }
+// globalVSlocal();
+// console.log('myName: global', myName);
