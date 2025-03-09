d745cd2dbd5062f959f7f819cf0a5c0fffbe8e17
shermuhammad786 <sherabro141@gmail.com>
2025-03-09 23:05:32 +0500

    class 11 js

---
+++ JavaScript/class 11/app.js
@@ -0,0 +1,52 @@
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var opr = prompt("Enter Operator");
+
+
+// switch (opr) {
+//     case '+':
+//         document.write(num1 + num2);
+//         break;
+//     case "-":
+//         document.write(num1 - num2)
+//         break;
+//     case "x":
+//         document.write(num1 * num2)
+//         break;
+//     case "/":
+//         document.write(num1 / num2);
+//         break;
+//     default:
+//         document.write("Enter a valid number")
+// }
+
+
+
+// if (opr == "+") {
+//     document.write(num1 + num2);
+// } else if (opr == "-") {
+//     document.write(num1 - num2)
+// } else if (opr == "x") {
+//     document.write(num1 * num2)
+// } else if (opr == "/") {
+//     document.write(num1 / num2)
+// } else {
+//     document.write("Enter a valid number")
+// }
+
+// for (var i = 0; i <= 3; i++) {
+//     alert(i);
+// }
+
+// var i = 0;
+// while (i <= 3) {
+//     alert(i);
+//     i++
+// }
+
+// var i = 5;
+// do {
+//     alert(i);
+//     console.log("i value is ", i);
+//     i++;
+// } while (i < 4); 
