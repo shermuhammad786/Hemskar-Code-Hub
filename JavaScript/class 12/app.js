6966fa945b9edc3f3c1131b8d3d5eed4a8e70e83
shermuhammad786 <sherabro141@gmail.com>
2025-03-11 23:16:17 +0500

    class 12 events

---
+++ JavaScript/class 12/app.js
@@ -0,0 +1,22 @@
+// function checkAddress() {
+//     var email = document.getElementById("email").value
+//     console.log("email =>>> ", email)
+//     // if (document.getElementById(fieldId).value === "") {
+//     //     alert("Email address required.");
+//     // }
+// }
+
+
+function signup() {
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("pass").value
+
+
+    console.log('name: ', name);
+    console.log('email: ', email);
+    console.log('password: ', password);
+
+    alert("Signup Successfully");
+
+}
