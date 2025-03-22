802ba089fb7500a27d04a4d0adbbfeef86d16da5
shermuhammad786 <sherabro141@gmail.com>
2025-03-22 23:42:56 +0500

    class 15

---
+++ JavaScript/class 15/app.js
@@ -0,0 +1,35 @@
+// var car = document.getElementById("swapCarImage");
+// car.src = "./car2.jpeg"
+
+
+// var email = document.getElementById("email");
+// var pass = document.getElementById("password");
+// var link = document.getElementById("loginToHome");
+
+
+
+// function logInHandler() {
+//     console.log("passowrd ==>>>", pass.value)
+//     console.log('email: ', email.value);
+
+//     if (email.value === "hemskar@gmail.com") {
+//         console.log("Email is correct");
+//         if (pass.value === "123") {
+//             console.log("signup successfull")
+//             link.href = "./home.html"
+//             alert("Signup successfull")
+//         } else {
+//             alert("Invalid Credentials")
+//         }
+//     } else {
+//         alert("User not found");
+//     }
+
+// }
+
+
+var div = document.getElementById("container");
+console.log('div: ', div);
+
+var para = div.getElementsByTagName("p")
+console.log('para: ', para);
